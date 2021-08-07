82802c462922a07bde0424a66e602c0a6a31ee63
Itskrish01 <72066631+Itskrish01@users.noreply.github.com>
2021-08-07 14:15:14 +0530

    Create Pyramid.py

---
+++ Pyramid.py
@@ -0,0 +1,29 @@
+def triangle(n):
+     
+    # number of spaces
+    k = n - 1
+ 
+    # outer loop to handle number of rows
+    for i in range(0, n):
+     
+        # inner loop to handle number spaces
+        # values changing acc. to requirement
+        for j in range(0, k):
+            print(end=" ")
+     
+        # decrementing k after each loop
+        k = k - 1
+     
+        # inner loop to handle number of columns
+        # values changing acc. to outer loop
+        for j in range(0, i+1):
+         
+            # printing stars
+            print("* ", end="")
+     
+        # ending line after each row
+        print("\r")
+ 
+# Changed str to int
+n = int(input("Enter row for your pyramid:-\t"))
+triangle(n)
